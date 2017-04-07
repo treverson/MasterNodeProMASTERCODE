9c51cc46dbe47973f56d7c66172a647d01e7fd9f
Yoshi <jason@htmllabs.com>
2017-04-07 10:56:42 -0400

    Update Layout Concept.

---
+++ resources/views/layout/logo.blade.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="col-md-12" style="text-align: center;">
+        <div style="display: inline-block;">
+            <img src="/img/ionmasternodes.png" class="logo">
+        </div>
+    </div>
+</div>
---
+++ resources/views/layout/statsbar.blade.php
@@ -0,0 +1,32 @@
+<div class="row bar">
+    <div class="col-md-12" style="text-align: center;">
+        <div class="col-md-1"></div>
+        <div class="col-md-10" style="display: inline-block;">
+            <div class="col-md-2 bardata">
+                <div class="bardatatitle">TOTAL MASTERNODES</div>
+                <div class="bardatadata">{!! $totalnodes[0]['total'] !!}</div>
+            </div>
+            <div class="col-md-2 bardata">
+                <div class="bardatatitle">REWARDS DAILY</div>
+                <div class="bardatadata">{!! number_format($block24total,'8','.','') !!}</div>
+            </div>
+            <div class="col-md-2 bardata">
+                <div class="bardatatitle">24HR BLOCK COUNT</div>
+                <div class="bardatadata">{!! $block24hour !!}</div>
+            </div>
+            <div class="col-md-2 bardata">
+                <div class="bardatatitle">INCOME DAILY</div>
+                <div class="bardatadata">$<span>{!! number_format($incomedaily,'2','.',',') !!}</span></div>
+            </div>
+            <div class="col-md-2 bardata">
+                <div class="bardatatitle">INCOME PER MONTH</div>
+                <div class="bardatadata">$<span>{!! number_format($incomemonth,'2','.',',') !!}</span></div>
+            </div>
+            <div class="col-md-2 bardataend">
+                <div class="bardatatitle">PRICE PER ION</div>
+                <div class="bardatadata">$<span>{!! number_format($price_usd,'2','.',',') !!}</span></div>
+            </div>
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+</div>
