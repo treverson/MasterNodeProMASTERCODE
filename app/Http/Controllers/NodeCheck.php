622153c25dc51b4beba7f051ad6956b855233f72
Yoshi <jason@htmllabs.com>
2017-04-28 16:11:12 -0400

    Speed up Site with Core Data Flow

---
+++ app/Http/Controllers/NodeCheck.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Blocks;
+use Validator, Input, Redirect, View, Auth;
+use App\Mnl;
+
+class NodeCheck extends Controller
+{
+
+	public function index($ip,$port) {
+
+
+	}
+}
