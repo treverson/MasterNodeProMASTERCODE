cc89b61cc7d2939bb5d7c7af6d22abc8350fd840
Yoshi <jason@htmllabs.com>
2017-05-03 12:16:12 -0400

    Update to statsbar

--- resources/views/layout/statsbar.blade.php
+++ resources/views/layout/statsbar.blade.php
@@ -1,44 +1,34 @@
 <div class="row bar">
-    <div class="col-md-12" style="text-align: center;">
-        <div class="col-md-1"></div>
-        <div class="col-md-10" style="display: inline-block;">
-            <div class="col-md-5"></div>
-            <div class="col-md-2">
-                <div class="bardatatitle">ION Price</div>
-                <div class="bardatadata">$<span class="green">{!! number_format($price_usd,'2','.',',') !!}</span></div>
-            </div>
-            <div class="col-md-5"></div>
-        </div>
-        <div class="col-md-1"></div>
-    </div>
-    <div class="col-md-12" style="text-align: center;">
-        <div class="col-md-1"></div>
-        <div class="col-md-10" style="display: inline-block;">
-            <div class="col-md-2 bardata">
-                <div class="bardatatitle">Total Masternodes</div>
-                <div class="bardatadata"><span class="orange">{!! $firstNode['total'] !!}</span></div>
-            </div>
-            <div class="col-md-2 bardata">
-                <div class="bardatatitle">Daily ION Earned</div>
-                <div class="bardatadata"><span class="blue">{!! number_format($iondaily,'2','.','') !!}</span></div>
-            </div>
-            <div class="col-md-2 bardata">
-                <div class="bardatatitle">Daily Income</div>
-                <div class="bardatadata">$<span class="green">{!! number_format($income['daily'],'2','.',',') !!}</span></div>
-            </div>
-            <div class="col-md-2 bardata">
-                <div class="bardatatitle">Weekly Income</div>
-                <div class="bardatadata">$<span class="green">{!! number_format($income['weekly'],'2','.',',') !!}</span></div>
-            </div>
-            <div class="col-md-2 bardata">
-                <div class="bardatatitle">Monthly Income</div>
-                <div class="bardatadata">$<span class="green">{!! number_format($income['monthly'],'2','.',',') !!}</span></div>
-            </div>
-            <div class="col-md-2 bardataend">
-                <div class="bardatatitle">Yearly Income</div>
-                <div class="bardatadata">$<span class="green">{!! number_format($income['yearly'],'2','.',',') !!}</span></div>
+    <div class="col-md-12 col-lg-12" style="text-align: center;">
+        <div class="col-lg-1 hidden-md"></div>
+        <div class="col-lg-10 col-md-12" style="display: inline-block;">
+            <div class="row">
+                <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6 bardata">
+                    <div class="bardatatitle">Total Masternodes</div>
+                    <div class="bardatadata"><span class="orange">{!! $firstNode['total'] !!}</span></div>
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6 bardata">
+                    <div class="bardatatitle">Daily ION Earned</div>
+                    <div class="bardatadata"><span class="blue">{!! number_format($iondaily,'2','.','') !!}</span></div>
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6 bardata">
+                    <div class="bardatatitle">Daily Income</div>
+                    <div class="bardatadata">$<span class="green">{!! number_format($income['daily'],'2','.',',') !!}</span></div>
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6 bardata">
+                    <div class="bardatatitle">Weekly Income</div>
+                    <div class="bardatadata">$<span class="green">{!! number_format($income['weekly'],'2','.',',') !!}</span></div>
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6 bardata">
+                    <div class="bardatatitle">Monthly Income</div>
+                    <div class="bardatadata">$<span class="green">{!! number_format($income['monthly'],'2','.',',') !!}</span></div>
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-6 col-xs-6 bardata">
+                    <div class="bardatatitle">Yearly Income</div>
+                    <div class="bardatadata">$<span class="green">{!! number_format($income['yearly'],'2','.',',') !!}</span></div>
+                </div>
             </div>
         </div>
-        <div class="col-md-1"></div>
+        <div class="col-lg-1 hidden-md"></div>
     </div>
 </div>
