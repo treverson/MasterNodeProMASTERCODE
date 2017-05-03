a18840394e2e08399375f4cc81e4f729b78d5322
Yoshi <jason@htmllabs.com>
2017-05-03 12:15:26 -0400

    Add ION Price to Logo Bar.

--- resources/views/layout/logo.blade.php
+++ resources/views/layout/logo.blade.php
@@ -1,7 +1,14 @@
-<div class="row">
-    <div class="col-md-12" style="text-align: center;">
+<div class="row"  style="text-align: center;">
+    <div class="col-lg-5 col-md-5 hidden-sm hidden-xs"></div>
+    <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
         <div style="display: inline-block;">
             <a href="/"><img src="/img/ionmasternodes.png" class="logo"></a>
         </div>
     </div>
+    <div class="col-lg-3 col-md-3 hidden-sm hidden-xs"></div>
+    <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+        <div class="bardatatitle">ION Price</div>
+        <div class="bardatadata">$<span class="green">{!! number_format($price_usd,'2','.',',') !!}</span></div>
+    </div>
+</div>
 </div>
