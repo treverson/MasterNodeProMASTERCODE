87e0db5f8617992c740c39d2ecf886b11c6f7c01
Yoshi <jason@htmllabs.com>
2017-07-01 13:33:09 -0400

    Update

--- public/checkblock.php
+++ public/checkblock.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'coins.php';
-$wallet = new jsonRPCClient('http://' . $chc['user'] . ':' . $chc['pass'] . '@'.$chc['ip'].':' . $chc['port'].'/',true);
+$wallet = new jsonRPCClient('http://' . $chc['user'] . ':' . $chc['pass'] . '@'.$chc['ip'].':' . $chc['port'].'/');
 if (isset($wallet)) {
 	try {
 		$blockhash = $wallet->getblockhash((int)$_REQUEST['block']);
