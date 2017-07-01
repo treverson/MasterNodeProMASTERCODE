0a33a9c347a11a7c2a5f8e1903d6a7d5531c6b92
Yoshi <jason@htmllabs.com>
2017-07-01 12:22:05 -0400

    TestWallet

---
+++ public/TestWallet.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'coins.php';
+//$wallet = new jsonRPCClient('http://' . $chc['user'] . ':' . $chc['pass'] . '@'.$chc['ip'].':' . $chc['port'].'/',true);
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+	CURLOPT_PORT => "58273",
+	CURLOPT_URL => 'http://' . $chc['user'] . ':' . $chc['pass'] . '@'.$chc['ip'].':' . $chc['port'].'/',
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "POST",
+	CURLOPT_POSTFIELDS => "{\"method\":\"getblockbynumber\",\"params\":[2],\"id\":1,\"jsonrpc\":\"2.0\"}",
+	CURLOPT_HTTPHEADER => array(
+		"cache-control: no-cache",
+		"content-type: application/json"
+	),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+	echo "cURL Error #:" . $err;
+} else {
+	echo $response;
+}
