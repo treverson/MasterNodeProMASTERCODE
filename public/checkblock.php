fbb93b692f19d8e403b4256acee77ba5bf7f6155
Yoshi <jason@htmllabs.com>
2017-07-01 13:34:31 -0400

    Update

--- public/checkblock.php
+++ public/checkblock.php
@@ -1,21 +1,22 @@
 <?php
 require_once 'coins.php';
-$wallet = new jsonRPCClient('http://' . $chc['user'] . ':' . $chc['pass'] . '@'.$chc['ip'].':' . $chc['port'].'/');
+$wallet = new jsonRPCClient('http://' . $chc['user'] . ':' . $chc['pass'] . '@' . $chc['ip'] . ':' . $chc['port'] . '/');
 if (isset($wallet)) {
 	try {
 		$blockhash = $wallet->getblockhash((int)$_REQUEST['block']);
-		$process = $wallet->getblock($blockhash);
-//		foreach ($process['tx'] as $key => $value) {
-//			$tranX = $wallet->gettransaction($value);
-//			if (isset($tranX['vout'])) {
-//				$process['trans'][$key]['tx']   = $value;
-//				foreach ($tranX['vout'] as $voutKey => $vout) {
-//					$process['trans'][$key]['vout'][$voutKey] = $vout;
-//				}
-//			}
-//		}
+		$process   = $wallet->getblock($blockhash);
+		foreach ($process['tx'] as $key => $value) {
+			$tranX = $wallet->gettransaction($value);
+			if (isset($tranX['vout'])) {
+				$process['trans'][$key]['tx'] = $value;
+				foreach ($tranX['vout'] as $voutKey => $vout) {
+					$process['trans'][$key]['vout'][$voutKey] = $vout;
+				}
+			}
+		}
 		echo json_encode($process, JSON_PRETTY_PRINT);
-	} catch (exception $e) {
+	}
+	catch (exception $e) {
 		echo $e;
 	}
 }
