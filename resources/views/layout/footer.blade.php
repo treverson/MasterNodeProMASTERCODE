856dce0f621c00b77b9609fa31e5b6aa7cdadcbf
Yoshi <jason@htmllabs.com>
2017-07-08 11:30:03 -0400

    Update Footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="navbar-fixed-bottom" style="text-align: center">
     <div>
         <div class="col-md-3 col-xs-12">
-            Help Make this software Better: <a href="https://github.com/MasterYoshiDev/CHCMasterNode">GitHub</a>
+            Help Make this software Better: <a href="https://github.com/CHCMasterNodeDev/CHCMasterNode">GitHub</a>
         </div>
         <div class="col-md-3 dateupdated hidden-xs hidden-sm">Data Updated: {!! $lastUpdated !!}</div>
         <div class="col-md-3 col-xs-12">Create a Masternode: <a href="http://www.chaincoin.org/how-to-set-up-masternodes/" target="_blank">chaincoin.org</a></div>
