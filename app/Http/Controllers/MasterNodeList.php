b3cad1f7dcba03ad6b232ffc82a7d019315632f7
Yoshi <jason@htmllabs.com>
2017-07-08 11:08:19 -0400

    Update MasterNodeList Reward Drop

--- app/Http/Controllers/MasterNodeList.php
+++ app/Http/Controllers/MasterNodeList.php
@@ -14,28 +14,28 @@ class MasterNodeList
 {
 	public function reward($height)
 	{
-		if ($height >= 700799) {
+		if ($height <= 700799) {
 			$ret['height']     = 700799;
 			$ret['reward']     = 16;
 			$ret['nextreward'] = 8;
 		} elseif ($height >= 1401599) {
-			$ret['height']     = 568622;
+			$ret['height']     = 1401599;
 			$ret['reward']     = 8;
 			$ret['nextreward'] = 4;
 		} elseif ($height >= 2102399) {
-			$ret['height']     = 1012098;
+			$ret['height']     = 2102399;
 			$ret['reward']     = 4;
 			$ret['nextreward'] = 2;
 		} elseif ($height >= 2803199) {
-			$ret['height']     = 1455574;
+			$ret['height']     = 2803199;
 			$ret['reward']     = 2;
 			$ret['nextreward'] = 1;
 		} elseif ($height >= 3503999) {
-			$ret['height']     = 3675950;
+			$ret['height']     = 3503999;
 			$ret['reward']     = 1;
 			$ret['nextreward'] = .5;
 		} elseif ($height >= 4204799) {
-			$ret['height']     = 3675950;
+			$ret['height']     = 4204799;
 			$ret['reward']     = .5;
 			$ret['nextreward'] = .25;
 		} else {
