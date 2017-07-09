421a3eb15cd0cba90f8a5bb9ebf35c7c64a9e55e
Yoshi <jason@htmllabs.com>
2017-07-09 00:06:42 -0400

    Adding Command Line Pull

--- routes/console.php
+++ routes/console.php
@@ -16,3 +16,8 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('datapull', function () {
+	$mnl = new MasterNodeList();
+	$this->comment($mnl->datapull());
+})->describe('Pulling Data');
