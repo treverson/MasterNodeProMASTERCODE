4370ed98237caa32cf1616f8733cf9e36976d67f
Yoshi <jason@htmllabs.com>
2017-07-08 20:55:53 -0400

    Update Footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,10 +1,10 @@
 <footer class="navbar-fixed-bottom" style="text-align: center">
     <div>
         <div class="col-md-3 col-xs-12">
-            Help Make this software Better: <a href="https://github.com/CHCMasterNodeDev/CHCMasterNode">GitHub</a>
+            Help Make this software Better: <a href="{!! env('GITHUB') !!}" target="_blank">GitHub</a>
         </div>
         <div class="col-md-3 dateupdated hidden-xs hidden-sm">Data Updated: {!! $lastUpdated !!}</div>
-        <div class="col-md-3 col-xs-12">Create a Masternode: <a href="http://www.chaincoin.org/how-to-set-up-masternodes/" target="_blank">chaincoin.org</a></div>
-        <div class="col-md-3 col-xs-12">Donate: CVZXfffMPTPFnUBAeVVWGHFhzEwmK6JrKs</div>
+        <div class="col-md-3 col-xs-12">Create a Masternode: <a href="{!! env('MASTERNODE_LINK') !!}" target="_blank">{!! env('MASTERNODE_LINK_NAME') !!}</a></div>
+        <div class="col-md-3 col-xs-12">Donate: {!! env('DONATION_ADDRESS') !!}</div>
     </div>
 </footer>
