79c0f380e08333788046bdbaee262eed4e4cce87
Yoshi <jason@htmllabs.com>
2017-07-08 23:22:53 -0400

    Updates

--- app/Http/Controllers/MasterNodeList.php
+++ app/Http/Controllers/MasterNodeList.php
@@ -415,7 +415,7 @@ class MasterNodeList extends coin
 		$ret['price_usd']      = $cmc[0]['price_usd'];
 		$ret['block24hour']    = Blocks::where('created_at', '>=', date("Y-m-d H:m:s", strtotime('-1 day')))->count();
 		$rewardb24total        = Blocks::where('created_at', '>=', date("Y-m-d H:m:s", strtotime('-1 day')))->sum('amt');
-		$ret['avgblocks']      = $ret['block24hour'] / $total;
+		$ret['avgblocks']      = ($total > 0) ? $ret['block24hour'] / $total : 0;
 		$ret['iondaily']       = (count($list) > 0) ? $rewardb24total / count($list) : 0;
 		$ret['incomedaily']    = $ret['iondaily'] * $ret['price_usd'];
 		$ret['incomeweekly']   = $ret['incomedaily'] * 7;
