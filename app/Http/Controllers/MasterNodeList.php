f7a3681f0760545670e10b505df476bdb2512260
Yoshi <jason@htmllabs.com>
2017-07-09 21:35:55 -0400

    Update to DataPack

--- app/Http/Controllers/MasterNodeList.php
+++ app/Http/Controllers/MasterNodeList.php
@@ -130,6 +130,7 @@ class MasterNodeList extends coin
 		$ret['masterNodeWorth']         = $this->masterNodeWorth($dataCore['firstNode']['price']);
 		$ret['height']                  = $block['blockid'];
 		$ret['reward']                  = $this->reward($block['blockid']);
+		$ret['lastUpdated']             = date('F j, Y, g:i a T');
 		$return['ret']                  = $ret;
 		$return['dataCore']             = $dataCore;
 		return $return;
