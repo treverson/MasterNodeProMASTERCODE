9c449fc3adec37b741131c189677f79ec334f176
JSponaugle <simdude@gmail.com>
2017-07-26 10:37:25 -0400

    Update masternodelist.php

--- public/masternodelist.php
+++ public/masternodelist.php
@@ -2,6 +2,6 @@
 require_once 'coins.php';
 $wallet = new jsonRPCClient('http://' . $coin['user'] . ':' . $coin['pass'] . '@'.$coin['ip'].':' . $coin['port'].'/');
 if (isset($wallet)) {
-	$process = $wallet->goldminenodelist('full');
+	$process = $wallet->masternodelist('full');
 	echo json_encode($process);
 }
