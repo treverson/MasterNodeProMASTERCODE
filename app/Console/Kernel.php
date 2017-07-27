8a68ac521cdf9255773b892ddf853ebaed5448f6
Yoshi <jason@htmllabs.com>
2017-07-27 14:58:01 -0400

    Update to Crontab

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
 	protected function schedule(Schedule $schedule)
 	{
 		$schedule->call('App\Http\Controllers\MasterNodeList@datapull')
-				 ->everyMinute();
+				 ->everyMinute()->name('datapull')->withoutOverlapping();
 	}
 
 	/**
