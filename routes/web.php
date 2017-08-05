699a8f8be9299ae7bf34839112f29154cb6d9ad7
Yoshi <jason@htmllabs.com>
2017-08-05 08:36:25 -0400

    Update to Routes

--- routes/web.php
+++ routes/web.php
@@ -17,26 +17,26 @@ Route::group(['prefix' => 'api', 'middleware' => 'throttle:2'], function () {
 	Route::get('/datapull', array('uses' => 'MasterNodeList@datapull'));
 	Route::get('/getcoins', array('uses' => 'MasterNodeList@cmcPrice'));
 });
-Route::get('/', array('as' => 'index', 'uses' => 'MasterNodeList@masternodelist'));
-Route::get('/advanced/list', array('as' => 'advlist', 'uses' => 'MasterNodeList@moreList'));
-Route::get('/advanced/stats', array('as' => 'advstats', 'uses' => 'MasterNodeList@moreStats'));
-Route::get('/advanced/map', array('as' => 'advmap', 'uses' => 'MasterNodeList@moreMap'));
-Route::get('/advanced/graph', array('as' => 'advgraph', 'uses' => 'MasterNodeList@moreLineGraphs'));
-Route::get('/advanced/graph/data/', array('as' => 'mlgdata', 'uses' => 'MasterNodeList@moreLineGraphsData'));
-Route::get('/nodedetails/', array('as' => 'nodedetails', 'uses' => 'MasterNodeList@nodeDetails'));
-Route::get('/blocks', array('uses' => 'MasterNodeList@blockprocess'));
-Route::get('/lastblock', array('uses' => 'MasterNodeList@lastblock'));
-Route::get('/mynode/{secret}/', array('uses' => 'MasterNodeList@myrpinode'));
+Route::get('/', array('as' => 'index', 'uses' => 'MasterNodeList@masternodelist'))->middleware('throttle:6');
+Route::get('/advanced/list', array('as' => 'advlist', 'uses' => 'MasterNodeList@moreList'))->middleware('throttle:6');
+Route::get('/advanced/stats', array('as' => 'advstats', 'uses' => 'MasterNodeList@moreStats'))->middleware('throttle:6');
+Route::get('/advanced/map', array('as' => 'advmap', 'uses' => 'MasterNodeList@moreMap'))->middleware('throttle:6');
+Route::get('/advanced/graph', array('as' => 'advgraph', 'uses' => 'MasterNodeList@moreLineGraphs'))->middleware('throttle:6');
+Route::get('/advanced/graph/data/', array('as' => 'mlgdata', 'uses' => 'MasterNodeList@moreLineGraphsData'))->middleware('throttle:6');
+Route::get('/nodedetails/', array('as' => 'nodedetails', 'uses' => 'MasterNodeList@nodeDetails'))->middleware('throttle:6');
+Route::get('/lastblock', array('uses' => 'MasterNodeList@lastblock'))->middleware('throttle:2');
 Route::any('/{coin}/block', function($coin) {
 	$cc = new \App\Http\Controllers\coincontrol();
 	$process = $cc->block($coin);
 	echo $process;
-});
-Route::post('/rpinode/', array('uses' => 'MasterNodeList@rpinode'));
+})->middleware('throttle:2');
 Route::any('/{coin}/blocknumber/{number}', function($coin,$number) {
 	$cc = new \App\Http\Controllers\coincontrol();
 	$process = $cc->blocknumber($number);
 	echo $process;
-});
+})->middleware('throttle:2');
 
-Route::get('/checknode/{ip}/{port}', array('uses' => 'NodeCheck@index'));
+//Route::get('/blocks', array('uses' => 'MasterNodeList@blockprocess'));
+//Route::get('/mynode/{secret}/', array('uses' => 'MasterNodeList@myrpinode'));
+//Route::post('/rpinode/', array('uses' => 'MasterNodeList@rpinode'));
+//Route::get('/checknode/{ip}/{port}', array('uses' => 'NodeCheck@index'));
